f21e36d0c94317c95497209919741ac332f835b3
Luckas <luckas.peters@gmail.com>
2025-01-29 16:13:09 +0100

    added extra recipes and gave basic card styling

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -4,9 +4,12 @@ import { Link } from "react-router-dom";
 function RecipeCard({ recipe }) {
   return (
     <div className="recipe-card">
-      <h3>{recipe.title}</h3>
-      <p>{recipe.description}</p>
-      <Link to={`/recipe/${recipe.id}`}>View Details</Link>
+      <figure><img src={recipe.img} alt={recipe.title +" image"} /> </figure>
+      <article className="cardContent">
+        <h3>{recipe.title}</h3>
+        <p>{recipe.description}</p>
+        <Link to={`/recipe/${recipe.id}`}>View Details</Link>
+      </article>
     </div>
   );
 }
